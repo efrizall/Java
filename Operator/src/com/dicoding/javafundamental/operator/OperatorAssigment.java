0e378922234d93381223285fe3c6ab0029ec0964
efrizall <rizal.ef340@gmail.com>
2019-10-09 23:38:18 +0700

    Operator

---
+++ Operator/src/com/dicoding/javafundamental/operator/OperatorAssigment.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental.operator;
+
+public class OperatorAssigment {
+    public static void main(String[] args) {
+        int mData1 = 20;
+        int mData2 = 19;
+        System.out.println("Data pada mData1 adalah --> " + mData1);
+        System.out.println("Data pada mData2 adalah --> " + mData2);
+    }
+}
