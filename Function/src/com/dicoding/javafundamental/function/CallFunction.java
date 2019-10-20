e724199da17a28ce6a4be40c5a6152fe529251fc
efrizall <rizal.ef340@gmail.com>
2019-10-20 22:01:16 +0700

    Function

---
+++ Function/src/com/dicoding/javafundamental/function/CallFunction.java
@@ -0,0 +1,12 @@
+package com.dicoding.javafundamental.function;
+
+public class CallFunction {
+    public static void main(String[] args) {
+        // Memanggil fungsi
+        cobaFungsi();
+    }
+
+    private static void cobaFungsi() {
+        System.out.println("Ini adalah sebuah fungsi");
+    }
+}
---
+++ Function/src/com/dicoding/javafundamental/function/Main.java
@@ -0,0 +1,8 @@
+package com.dicoding.javafundamental.function;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
