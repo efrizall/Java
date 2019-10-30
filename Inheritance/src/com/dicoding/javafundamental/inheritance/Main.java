9552cd894ff636e0f2f0c04f9a036e4a656e2788
efrizall <rizal.ef340@gmail.com>
2019-10-30 22:29:58 +0700

    Inheritance

---
+++ Inheritance/src/com/dicoding/javafundamental/inheritance/Main.java
@@ -0,0 +1,8 @@
+package com.dicoding.javafundamental.inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
--- InterfaceImplement/src/com/dicoding/javafundamental/oop/Kucing.java
+++ InterfaceImplement/src/com/dicoding/javafundamental/oop/Kucing.java
@@ -1,8 +1,13 @@
 package com.dicoding.javafundamental.oop;
 
 public class Kucing implements Hewan {
+
+    /* Jika kita ingin mengimplementasikan interface maka kita
+    harus membuat method signature (nama, parameter, return)
+    yang sama disertai isi kode program untuk metodenya.
+     */
     @Override
     public void makan(){
-        
+
     }
 }
